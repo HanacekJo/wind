2f7731fcdaeedc8fecfcac032b0879701f404f1c
Josef Hanáček <josef.hanacek@zkl.cz>
2022-02-10 15:36:58 +0100

    vytvoření knihovny Logic a její napojení na konzoli, načtení souboru

---
+++ Logic/FileProcesor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class FileProcesor
+    {
+        public string Path { get; set; }
+
+        public void LoadFile()
+        {
+            Console.WriteLine($"Načítání souboru z cesty:\n{Path}");
+        }
+        // Private - přístupné pouze v rámci třídy, Public - přístupný z celého Solution Inrternal - v rámci projektu
+    }
+}
--- Wind.UI/Program.cs
+++ Wind.UI/Program.cs
@@ -1,4 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Logic;
+
 Console.WriteLine("Hello, World!");
 
 // komentar
+double number = 15;
+number += 5;
+number++;
+string text = "Mira";
+Console.WriteLine("Nubber:" + number + " mm");
+Console.WriteLine($"Nubber: {number}");
+FileProcesor fileProcesor = new FileProcesor();
+// Vytvořím instanci (objekt) třídy FileProcessor s názvem fileProcessor
+// new FileProcesor() = kontruktor
+fileProcesor.Path = @"C:\Users\josef.hanacek\source\repos\Wind.UI\Wind.UI\files";
+fileProcesor.LoadFile();
+Console.ReadLine();
